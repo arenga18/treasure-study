3b637a6733984bcc56ef88c3347cb5b897c4f921
arenga <arengapinnatamarzuki@gmail.com>
2024-11-22 21:07:47 +0700

    update build

--- resources/views/pages/grafik.blade.php
+++ resources/views/pages/grafik.blade.php
@@ -6,7 +6,8 @@
   <div class="mt-28">
     <section class="py-14 bg-gray-100">
       <div class="container mx-auto px-4">
-        <h1 id="judul" class="text-center font-bold text-2xl md:text-4xl mb-16 text-blue-600">Grafik Lulusan SMA
+        <h1 id="judul" class="text-center font-bold text-2xl md:text-4xl mb-14 lg:mb-20 text-blue-600">Grafik Lulusan
+          SMA
           Labschool Cirendeu</h1>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
           {{-- Grafik 2023 --}}
@@ -16,7 +17,7 @@
               SMA Labschool Cirendeu 2023
             </h2>
             <img src="{{ asset('img/grafik/2023.png') }}"
-              class="w-full h-[300px] lg:h-[450px] bg-contain mix-blend-multiply">
+              class="w-full h-[320px] lg:h-[460px] bg-auto bg-blend-color-burn object-contain">
           </div>
 
           {{-- Grafik 2024 --}}
@@ -26,7 +27,7 @@
               SMA Labschool Cirendeu 2024
             </h2>
             <img src="{{ asset('img/grafik/2024.png') }}"
-              class="w-full h-[300px] lg:h-[450px] bg-contain mix-blend-multiply">
+              class="w-full h-[320px] lg:h-[460px] bg-auto bg-blend-color-burn object-contain">
           </div>
         </div>
       </div>
